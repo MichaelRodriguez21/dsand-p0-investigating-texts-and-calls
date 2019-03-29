36eca202f4523ec3cda0be7fc9a4219b720cf867
Mostafa Elsheikh <sasa94s@msn.com>
2019-03-29 20:27:46 +0200

    feat: get telephone number which made the longest call

--- Task2.py
+++ Task2.py
@@ -3,6 +3,7 @@ Read file into texts and calls.
 It's ok if you don't understand how to read files
 """
 import csv
+from datetime import datetime
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
@@ -19,4 +20,12 @@ Print a message:
 "<telephone number> spent the longest time, <total time> seconds, on the phone during 
 September 2016.".
 """
+# initalizing a tuple of three items (telephone number, call duration, start of telephone call)
+longest_call = (calls[0][1], int(calls[0][3]), calls[0][2])
 
+for call in calls:
+    if longest_call[1] < int(call[3]):
+        longest_call = (call[1], int(call[3]), call[2])
+
+telephone_formatted_date = datetime.strftime(datetime.strptime(longest_call[2], '%d-%m-%Y %H:%M:%S'), '%B %Y')
+print("{} spent the longest time, {} seconds, on the phone during {}.".format(longest_call[0], longest_call[1], telephone_formatted_date))
