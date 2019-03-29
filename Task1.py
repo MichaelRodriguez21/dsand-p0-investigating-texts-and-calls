91ed9550744e44b20eafcec33cc0acc8f307d150
Mostafa Elsheikh <sasa94s@msn.com>
2019-03-29 20:03:05 +0200

    feat: get different telephone numbers from logs

--- Task1.py
+++ Task1.py
@@ -18,3 +18,18 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+unique_tel_numbers = []
+
+for text in texts:
+    if text[0] not in unique_tel_numbers:
+        unique_tel_numbers.append(text[0])
+    if text[1] not in unique_tel_numbers:
+        unique_tel_numbers.append(text[1])
+
+for call in calls:
+    if call[0] not in unique_tel_numbers:
+        unique_tel_numbers.append(call[0])
+    if call[1] not in unique_tel_numbers:
+        unique_tel_numbers.append(call[1])
+
+print("There are {} different telephone numbers in the records.".format(len(unique_tel_numbers)))
